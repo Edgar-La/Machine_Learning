8e84896b9620cfcb3cf9c20a17c93e013e4b1edd
Edgar Lara <65983215+Edgar-La@users.noreply.github.com>
2021-01-31 17:46:01 -0600

    Changes in plot legends

--- k-Means_Clustering/ML_LaraArellano_Prac1_kMeans.py
+++ k-Means_Clustering/ML_LaraArellano_Prac1_kMeans.py
@@ -1,27 +1,25 @@
 #Edgar Lara Arellano
-#Algoritmo de Clustering k-Means
+#Algoritmo para Clustering
 
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
-from sklearn.cluster import KMeans
 import numpy as np
 from random import randint
 from math import dist
-import os
-#os.system('clear')
+#import os; os.system('clear')	#A veces uso el comando para limpiar la terminal
 
 
-#Hay k=3 grupos, y n=200 puntos
-#Se hacen globales las variables para poderlas usar en las siguientes funciones
+#Hay --k-- centros y --n-- puntos
+#Se hacen globales las variables para poderlas usar en cualquier funcion
 global k_CENTROS; global n_MUESTRAS; 
-#k_CENTROS = 3; n_MUESTRAS = 300;
-k_CENTROS = 3; n_MUESTRAS = 200;
+k_CENTROS = 3; n_MUESTRAS = 299;
+#k_CENTROS = 3; n_MUESTRAS = 200;
 
 
 #Funcion que llama al metodo -make_blobs()- visto en clase
 def crear_puntos():
-	#X, y_true = make_blobs(n_samples=n_MUESTRAS, centers=k_CENTROS, cluster_std=.90, random_state=3)
-	X, y_true = make_blobs(n_samples=n_MUESTRAS, centers=k_CENTROS, cluster_std=1, random_state=1)
+	X, y_true = make_blobs(n_samples=n_MUESTRAS, centers=k_CENTROS, cluster_std=.90, random_state=3)
+	#X, y_true = make_blobs(n_samples=n_MUESTRAS, centers=k_CENTROS, cluster_std=1, random_state=1)
 	return X
 
 
@@ -32,9 +30,6 @@ def indicar_centroides_iniciales(X):
 		pos_random = randint(0, n_MUESTRAS) #entero aleatorio de 0 a n
 		centros_random.append(X[pos_random]) #guardo en un vector la posicion aleatoria
 	centros_random = np.array(centros_random)
-	#print(type(centros_random))
-	'''centros_random = np.array([X[197], X[198], X[199]])
-	print(centros_random); print(type(centros_random))'''
 	return centros_random
 
 
@@ -71,9 +66,10 @@ def recalcular_centroides(gamma, X):
 
 
 def lograr_convergencia(X, centros_random, gamma, centros_recalculados_):
-	vector_aux = []; contador = 1; check = 'Not Yet'; centros_recalculados = centros_recalculados_.copy()
+	global cont_iteraciones
+	vector_aux = []; cont_iteraciones = 1; check = 'Not Yet'; centros_recalculados = centros_recalculados_.copy()
 	while check != 'convergencia':
-		contador += 1
+		cont_iteraciones += 1
 		#Repito las funciones de membresia y de calculo
 		gamma = asignar_membresia(X, centros_recalculados)
 		centros_recalculados, cant_puntos_cluster = recalcular_centroides(gamma, X)
@@ -81,7 +77,7 @@ def lograr_convergencia(X, centros_random, gamma, centros_recalculados_):
 		if (np.array_equal(vector_aux, centros_recalculados) == True):
 			#Si converge termina el ciclo
 			check = 'convergencia'
-			print('\nIteraciones del algoritmo = ', contador)
+			print('\nIteraciones del algoritmo = ', cont_iteraciones)
 			print('\nCentros iniciales random = \n', centros_random)
 			print('\nCentros recalculados = \n', centros_recalculados)
 			print('\nLos puntos en c/centroide = \n', cant_puntos_cluster)
@@ -112,30 +108,33 @@ def etiquetas_cluster_plot(cant_puntos_cluster):
 
 #Funcion para ajustes del gráfico
 def graficar_puntos_generados(X, centros_random, centros_recalculados, clusters, labels_cluster):
+	global cont_iteraciones
 	f = plt.figure(figsize=(15,4.5))
 	puntos_originales = f.add_subplot(121)
 	puntos_con_centroides = f.add_subplot(122)
 
+	#Grafica de la izquuierda: ----Random----
 	puntos_originales.scatter(X[:,0], X[:,1], s=50, label = 'Puntos aleatorios')
 	puntos_originales.scatter(centros_random[:,0], centros_random[:,1], c='black', s=200, alpha = .5, label = 'Centros aleatorios')
 	puntos_originales.legend()
 
-	#Uso for para que agregue etiquetas por c/cluster
+	#Grafica de la derecha: ----Recalculado----
+	#Para poner 2 leyendas (una de colores) tuve que buscar documentacion: https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/scatter_with_legend.html
 	f_1 = puntos_con_centroides.scatter(X[:,0], X[:,1], c=clusters, s=50, cmap='spring')
-	l_1 = puntos_con_centroides.legend(handles=f_1.legend_elements()[0], labels=labels_cluster, loc ="upper left")
 	f_2 = puntos_con_centroides.scatter(centros_recalculados[:,0], centros_recalculados[:,1], c='black', s=200, alpha = .5, label = 'Centroides recalculados')
-	#l_2 = puntos_con_centroides.legend(handles=f_2.legend_elements()[0], labels = 'Centroides recalculados', loc = 'lower right')
-
-
-
+	l_1 = puntos_con_centroides.legend(handles = f_1.legend_elements()[0], labels=labels_cluster, title = 'Clusters', loc='lower right')
+	l_2 = puntos_con_centroides.legend()
+	puntos_con_centroides.add_artist(l_1)
+	puntos_con_centroides.add_artist(l_2)
+	puntos_con_centroides.title.set_text('Iteraciones del algoritmo: ' + str(cont_iteraciones))
 	plt.show()	
 
 
 
 
-
-
 #Sección donde se llama a las funciones
+#Se trató de dividir según los pasos del pseudocódigo mostrado en clase
+########################################################################################################################
 X = crear_puntos()
 centros_random = indicar_centroides_iniciales(X)
 gamma_ = asignar_membresia(X, centros_random)
@@ -143,6 +142,6 @@ centros_recalculados_, cant_puntos_cluster = recalcular_centroides(gamma_, X)
 #Esta funcion es importante va a repetir las membresias y calculos hasta lograr una convergencia
 centros_recalculados, gamma, cant_puntos_cluster =  lograr_convergencia(X, centros_random, gamma_, centros_recalculados_)
 clusters = colores_cluster(gamma)
-
 labels_cluster = etiquetas_cluster_plot(cant_puntos_cluster)
 graficar_puntos_generados(X, centros_random, centros_recalculados, clusters, labels_cluster)
+input()
