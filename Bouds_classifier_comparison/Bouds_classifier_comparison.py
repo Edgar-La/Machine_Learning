549f1a14dab6cd87fa0b7c4f4415ed92193c9191
Edgar-La <laraae2018@licifug.ugto.mx>
2021-04-25 14:31:15 -0500

    Cleanig code

--- Bouds_classifier_comparison/Bouds_classifier_comparison.py
+++ Bouds_classifier_comparison/Bouds_classifier_comparison.py
@@ -2,37 +2,43 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Open a csv file with a random matrix
-def open_file(name_file):
-  return  pd.read_csv(name_file)
 
 #Convert pandas datframe to numpy matrix
 def dataframe_to_matrix(df):
   return np.array(df)
 
 def generate_datasets(datasets):
-	df_1 = open_file(datasets[0])
-	df_2 = open_file(datasets[1])
-	df_3 = open_file(datasets[2])
+	dataset_1 = np.array(pd.read_csv(datasets[0], index_col = 0))
+	dataset_2 = np.array(pd.read_csv(datasets[1], index_col = 0))
+	dataset_3 = np.array(pd.read_csv(datasets[2]))
+	return [dataset_1, dataset_2, dataset_3]
 
-	return [dataframe_to_matrix(df_1), dataframe_to_matrix(df_2), dataframe_to_matrix(df_3)]
+def clean_datasets(datasets):
+	X = [[],[],[]]
+	for k in range(len(datasets)):
+		for n in range(len(datasets[k])):
+			X[k].append([datasets[k][n][0],datasets[k][n][1]])
+		X[k] = np.array(X[k])
+	y_label = [datasets[0][:,2], datasets[1][:,2], datasets[2][:,2]]
+	return X, y_label
 
-def plotter_function(matrix_1, matrix_2, matrix_3, datasets_names):
-  plt.figure(figsize=(5, 5))
 
-  plt.subplot(341)
-  plt.scatter(matrix_1[:,1], matrix_1[:,2],  c=matrix_1[:,3], s=2, cmap=colormap)
-  plt.title(datasets_names[0])
 
-  plt.subplot(345)
-  plt.scatter(matrix_2[:,1], matrix_2[:,2],  c=matrix_2[:,3], s=2, cmap=colormap)
-  plt.title(datasets_names[1])
+def plotter_function(X, y_label, datasets_names):
+	plt.figure(figsize=(5, 5))
+	plt.subplot(341)
+	plt.scatter(X[0][:,0], X[0][:,1],  c=y_label[0], s=2, cmap=colormap)
+	plt.title(datasets_names[0])
 
-  plt.subplot(349)
-  plt.scatter(matrix_3[:,0], matrix_3[:,1],  c=matrix_3[:,2], s=2, cmap=colormap)
-  plt.title(datasets_names[2])
+	plt.subplot(345)
+	plt.scatter(X[1][:,0], X[1][:,1],  c=y_label[1], s=2, cmap=colormap)
+	plt.title(datasets_names[1])
 
-  plt.show()
+	plt.subplot(349)
+	plt.scatter(X[2][:,0], X[2][:,1],  c=y_label[2], s=2, cmap=colormap)
+	plt.title(datasets_names[2])
+
+	plt.show()
 
 datasets_names = ['dataset_classifiers1.csv', 'dataset_classifiers2.csv', 'dataset_classifiers3.csv']
 colormap = plt.cm.RdYlBu
@@ -40,4 +46,6 @@ names = ['MEDC', 'k-NN', 'SVC']
 
 
 datasets = generate_datasets(datasets_names)
-plotter_function(datasets[0], datasets[1], datasets[2], names)
+X, y_label = clean_datasets(datasets)
+#print(y_label[2])
+plotter_function(X,y_label, names)
